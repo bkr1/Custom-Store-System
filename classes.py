e4fc8eae506fc8dc00586a07fabb222aa4474eaf
bkr1 <lucasnovais222@gmail.com>
2020-04-07 14:48:19 -0300

    Adicionando os codigos e arquivos ate entao desenvolvidos.

---
+++ classes.py
@@ -0,0 +1,110 @@
+class User:
+    def __init__(self, name, email, login, password):
+        self.__name = name
+        self.__email = email
+        self.__login = login
+        self.__password = password
+
+    def __str__(self):
+        return '\n\tName: %s\n\tEmail: %s\n\tLogin: %s\n\t' %(self.name, self.email, self.login)
+    def write(self):
+        return 'n=%s\ne=%s\nl=%s\np=%s' %(self.name, self.email, self.login, self.password)
+
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def email(self):
+        return self.__email
+    @property
+    def login(self):
+        return self.__login
+    @property
+    def password(self):
+        return self.__password
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+    @email.setter
+    def email(self, email):
+        self.__email = email
+    @login.setter
+    def login(self, login):
+        self.__login = login
+    @password.setter
+    def password(self, password):
+        self.__password = password
+
+class Product:
+    def __init__(self, name, brand, category, price, code, stock, user):
+        self.__name = name
+        self.__brand = brand
+        self.__category = category
+        self.__price = price
+        self.__code = code
+        self.__salesThisMonth = 0
+        self.__stock = stock
+        self.__user = user
+        #self.__totalSales = 0
+
+    def write(self):
+        return 'n=%s\nb=%s\nct=%s\np=%f\nc=%s\ns=%d\nu=%s\n' %(self.name, self.brand, self.category, self.price, self.code, self.stock, self.user)
+    def writeCode(self):
+        return '%s\n' %(self.code)
+
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def brand(self):
+        return self.__brand
+    @property
+    def category(self):
+        return self.__category
+    @property
+    def price(self):
+        return self.__price
+    @property
+    def code(self):
+        return self.__code
+    @property
+    def salesThisMonth(self):
+        return self.__salesThisMonth
+    @property
+    def stock(self):
+        return self.__stock
+    @property
+    def user(self):
+        return self.__user
+    #@property
+    #def __totalSales(self):
+    #    return self.__totalSales
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+    @brand.setter
+    def brand(self, brand):
+        self.__brand = brand
+    @category.setter
+    def category(self, category):
+        self.__category = category
+    @price.setter
+    def price(self, price):
+        self.__price = price
+    @code.setter
+    def code(self, code):
+        self.__code = code
+    @salesThisMonth.setter
+    def salesThisMonth(self, sales):
+        self.__salesThisMonth = sales
+    @stock.setter
+    def stock(self, stock):
+        self.__stock = stock
+    @user.setter
+    def user(self, user):
+        self.__user = user
+    #@totalSales.setter
+    #def totalSales(self, sales):
+    #   self.__totalSales = sales
---
+++ main.py
@@ -0,0 +1,291 @@
+import os
+import sys
+import getpass
+from getch import getch
+from classes import User, Product
+
+currentUser = None
+
+#Hide the password with '*';
+def win_getpass(prompt='Password: ', stream=None):
+    """Prompt for password with echo off, using Windows getch()."""
+    if sys.stdin is not sys.__stdin__:
+        return fallback_getpass(prompt, stream)
+    import msvcrt
+    for c in prompt:
+        msvcrt.putwch(c)
+    pw = ""
+    while 1:
+        c = msvcrt.getwch()
+        if c == '\r' or c == '\n':
+            break
+        if c == '\003':
+            raise KeyboardInterrupt
+        if c == '\b':
+            if pw == '':
+                pass
+            else:
+                pw = pw[:-1]
+                msvcrt.putwch('\b')
+                msvcrt.putwch(" ")
+                msvcrt.putwch('\b')
+        else:
+            pw = pw + c
+            msvcrt.putwch("*")
+    msvcrt.putwch('\r')
+    msvcrt.putwch('\n')
+    return pw
+
+def firstScreen():
+    os.system('cls')
+    print('\t########## CUSTOM STORE - WELCOME ##########')
+    print('\n\tWelcome to the Custom Store storage service!')
+    print('\n\t\t1  Login')
+    print('\n\t\t2  Register')
+
+    try:
+        option = int(input('\nType an option: '))
+
+        if option == 1:
+            loginScreen()
+        elif option == 2:
+            registerScreen()
+        else:
+            print('\nATTENTION: Type a valild value!')
+            os.system('pause')
+            firstScreen()    
+    except ValueError:
+        print('\nATTENTION: Type a valild value!')
+        os.system('pause')
+        firstScreen()
+ 
+def loginScreen():
+    os.system('cls')
+    print('\t########## CUSTOM STORE - LOGIN ##########')
+    userLogin = input('\n\tLogin: ')
+
+    if not checkLogin(userLogin):
+        print('\n\tATTENTION: The login is NOT registered!')
+        os.system('pause')
+        firstScreen()
+
+    userPassword = win_getpass('\n\tPassword: ')
+
+    if not checkPassword(userLogin, userPassword):
+        print('\n\tATTENTION: The login and password does not match!')
+        os.system('pause')
+        firstScreen()
+
+    global currentUser
+
+    currentUser = userLogin
+    storeLayout()
+
+def password():
+    userPassword = win_getpass('\n\tPassword: ')
+    rePassword = win_getpass('\n\tRetype password: ')
+
+    if rePassword != userPassword:
+        print('\n\tATTENTION: The passwords does not match!')
+        password()
+
+    return userPassword
+
+def registerScreen():
+    os.system('cls')
+    print('\t########## CUSTOM STORE - REGISTER ##########')
+    userName = input('\n\tFull name: ')
+    userEmail = input('\n\tE-mail: ')
+    userLogin = input('\n\tLogin: ')
+
+    if checkLogin(userLogin):
+        print('\n\tATTENTION: The login is already registered!')
+        os.system('pause')
+        registerScreen()
+
+    userPassword = password()
+    
+    try:
+        check = input('\nATTENTION: Are the data entered correct? (y/n) ')
+
+        if check == 'y':
+            createUser(userName, userEmail, userLogin, userPassword)
+        elif check == 'n':
+            registerScreen()
+        else:
+            print('\nATTENTION: Type a valild value!')
+            os.system('pause')
+            registerScreen() 
+    except ValueError:
+        print('\nATTENTION: Type a valild value!')
+        os.system('pause')
+        registerScreen()
+
+def createUser(userName, userEmail, userLogin, userPassword):
+    newUser = User(userName, userEmail, userLogin, userPassword)
+    writeUsers(newUser)
+    print('\n\tUser SUCCESSFULLY created!')
+    os.system('pause')
+    firstScreen()
+
+def writeUsers(user):
+    with open('_files\data.txt', 'a+') as f:
+        f.write(user.write() + '\n')
+
+def checkLogin(login):
+    login = 'l=' + login
+    try:
+        with open('_files\data.txt', 'r') as f:
+            userdata = f.readlines()
+            for line in userdata:
+                if login in line:
+                    return True
+        return False
+    except FileNotFoundError:
+        print('\n\tERROR: Data file not found!')
+        exit()
+
+def checkPassword(login, password):
+    password = 'p=' + password
+    login = 'l=' + login + '\n'
+    prevLine = -1
+    try:
+        with open('_files\data.txt', 'r') as f:
+            userdata = f.readlines()
+            for line in userdata:
+                if password in line and prevLine == login:
+                    os.system('pause')
+                    return True
+                prevLine = line
+        return False
+    except FileNotFoundError:
+        print('\n\tERROR: Data file not found!')
+        exit()
+
+def storeLayout():
+    os.system('cls')
+    print('\t########## CUSTOM STORE ##########')
+    print('\n\t\t1  Products')
+    print('\n\t\t2  Employees')
+    print('\n\t\t3  Clients')
+    print('\n\t\t4  Sales')
+    print('\n\t\t5  Quit')
+
+    try:
+        op = int(input('\nType an option: '))
+
+        if op == 1:
+            productsMenu()
+        elif op == 2:
+            pass
+        elif op == 3:
+            pass
+        elif op == 4:
+            pass
+        elif op == 5:
+            print('\n\tLeaving the application...')
+            os.system('pause')
+            exit()
+        else:
+            print('\nATTENTION: Type a valild value!')
+            os.system('pause')
+            storeLayout()
+
+    except ValueError:
+        print('\nATTENTION: Type a valild value!')
+        os.system('pause')
+        storeLayout()
+    
+def productsMenu():
+    os.system('cls')
+    print('\t########## CUSTOM STORE - PRODUCTS ##########')
+    print('\n\t\t1  Search')
+    print('\n\t\t2  Sell')
+    print('\n\t\t3  Register')
+    print('\n\t\t4  Previous Menu')
+    
+    try:
+        op = int(input('\nType an option: '))
+
+        if op == 1:
+            searchProductsMenu()
+        elif op == 3:
+            regiterProductsMenu()
+        elif op == 4:
+            storeLayout()
+    except ValueError:
+        print('\nATTENTION: Type a valild value!')
+        os.system('pause')
+        productsMenu()
+
+def searchProductsMenu():
+    os.system('cls')
+    print('\t########## CUSTOM STORE - SEARCH PRODUCTS ##########')
+    print('\n\t\t1  Search for Name')
+    print('\n\t\t2  Search for Code')
+    print('\n\t\t3  Search for Branch')
+    print('\n\t\t4  Search for Stock')
+    print('\n\t\t5  Previous Menu')
+
+    try:
+        op = int(input('\nType an option: '))
+    except ValueError:
+        print('\nATTENTION: Type a valild value!')
+        os.system('pause')
+        productsMenu()
+
+def regiterProductsMenu():
+    os.system('cls')
+    print('\t########## CUSTOM STORE - REGISTER PRODUCTS ##########')
+    print("\n\tPress 'i' if you want to review the Register Product rules;")
+    info = getch()
+
+    if info == 'i':
+        pass
+
+    try:
+        name = input('\n\tProduct Name: ')
+        brand = input('\n\tProduct Brand: ')
+        code = input('\n\tProduct Code: ')
+
+        if checkCode(code):
+            print('\nATTENTION: The product is already registered!')
+            os.system('pause')
+            productsMenu()
+
+        category = input('\n\tProduct Category: ')
+        price = float(input('\n\tProduct Price: '))
+        n = int(input('\n\tNumber of products: '))
+        user = currentUser
+        createProduct(name, brand, code, category, price, n, user)
+    except ValueError:
+        print('\nATTENTION: Type a valid value!')
+        os.system('pause')
+        regiterProductsMenu()
+
+def checkCode(code):
+    code = code + '\n'
+    try:
+        with open('_files\codes.txt', 'r') as f:
+            userdata = f.readlines()
+            for line in userdata:
+                if code in line:
+                    return True
+        return False
+    except FileNotFoundError:
+        print('\n\tERROR: Data file not found!')
+        exit()
+
+def createProduct(name, brand, code, category, price, n, user):
+    product = Product(name, brand, category, price, code, n, user)
+    registerProduct(product)
+
+def registerProduct(product):
+    with open('_files\products.txt', 'a+') as f:
+        f.write(product.write())
+    with open('_files\codes.txt', 'a+') as f:
+        f.write(product.writeCode())
+    
+    print('\n\tProduct SUCCESSFULLY registered!')
+
+firstScreen()
